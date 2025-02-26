56491d2c38c28d3a46a43ead4f88cc62137c35fe
xinshen <contact@mindblossom.net>
2025-02-26 22:03:35 +0100

    update

--- queries/insert.py
+++ queries/insert.py
@@ -1,35 +1,40 @@
-def create_nodes(tx, infos):
+from typing import Dict, Optional
 
-       host = infos['host_info']
+from neo4j import Transaction
 
-       if infos['port_info']:
-              ports = infos['port_info']
-              for port in ports:
-                     insert_host_with_port(tx, host, port)
-       else:
-              insert_host_only(tx, host)
 
-def insert_host_only(tx, host):
+def create_nodes(tx: Transaction, infos: Dict[str, Dict[str, Optional[str]]]) -> None:
+    host = infos["host_info"]
 
-       tx.run(
-              "MERGE(h:Host {ip: $ip, hostname: $hostname})",
-              hostname=host['hostname'],
-              ip=host['ip']
-       )
+    if infos["port_info"]:
+        ports = infos["port_info"]
+        for port in ports:
+            insert_host_with_port(tx, host, port)
+    else:
+        insert_host_only(tx, host)
 
-def insert_host_with_port(tx, host, port):
-              
-       tx.run(
-              "MERGE(p:Port {port: $port, state: $state, protocol: $protocol, service: $service, sunrpcinfo: $sunrpcinfo, versioninfo: $versioninfo})"
-              "MERGE(h:Host {ip: $ip, hostname: $hostname})"
-              "MERGE (p)-[:OPEN]->(h)",
-              port=port['no'],
-              state=port['state'],
-              protocol=port['protocol'],
-              service=port['service'],
-              sunrpcinfo=port['sunrpcinfo'],
-              versioninfo=port['versioninfo'],
-              hostname=host['hostname'],
-              ip=host['ip']
-       )
-              
+
+def insert_host_only(tx: Transaction, host: Dict[str, str]) -> None:
+    tx.run(
+        "MERGE(h:Host {ip: $ip, hostname: $hostname})",
+        hostname=host["hostname"],
+        ip=host["ip"],
+    )
+
+
+def insert_host_with_port(
+    tx: Transaction, host: Dict[str, str], port: Dict[str, str]
+) -> None:
+    tx.run(
+        "MERGE(p:Port {port: $port, state: $state, protocol: $protocol, service: $service, sunrpcinfo: $sunrpcinfo, versioninfo: $versioninfo})"
+        "MERGE(h:Host {ip: $ip, hostname: $hostname})"
+        "MERGE (p)-[:OPEN]->(h)",
+        port=port["no"],
+        state=port["state"],
+        protocol=port["protocol"],
+        service=port["service"],
+        sunrpcinfo=port["sunrpcinfo"],
+        versioninfo=port["versioninfo"],
+        hostname=host["hostname"],
+        ip=host["ip"],
+    )
